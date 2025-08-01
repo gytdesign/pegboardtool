e8b3fb1179b6f5da478c08c9f7b8f650aaf90964
Yu Tong <80372671+gytdesign@users.noreply.github.com>
2025-08-01 16:56:01 +0800

    removed styling in div to fix error of '

--- pages/pegboard.js
+++ pages/pegboard.js
@@ -1,6 +1,6 @@
 export default function Pegboard() {
   return (
-    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+    <div>
       <h1>Pegboard Customiser</h1>
       <p>If you're seeing this, the route is working!</p>
     </div>
