40406e4b56dd6edec34622e92c9a0d2400e1b1b3
Yu Tong <80372671+gytdesign@users.noreply.github.com>
2025-08-01 17:03:46 +0800

    removed .eslintrc.json and replaced ' with the code &apos;

--- pages/pegboard.js
+++ pages/pegboard.js
@@ -2,7 +2,7 @@ export default function Pegboard() {
   return (
     <div>
       <h1>Pegboard Customiser</h1>
-      <p>If you're seeing this, the route is working!</p>
+      <p>If you&apos;re seeing this, the route is working!</p>
     </div>
   );
 }
